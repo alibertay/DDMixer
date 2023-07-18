8306a91d01b3820f8dfebe75019e3760751dfb1a
alibertay.solak <alibertay.solak@SOFTLAB-8>
2023-07-18 16:17:17 +0300

    Pop-up added, hash function created

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
 
+@app.route('/popup', methods=['POST'])
+def popup():
+    name = request.form['name']
+    return jsonify({'name': name})
+
+@app.route('/get_message', methods=['POST'])
+def get_message():
+    name = request.form['name']
+    message = hashWithKeccak(name)
+    return jsonify({'message': message})
+
+def hashWithKeccak(name):
+    # TODO: connect to contract and hash name with keccak
+    name = name + "iuv"
+    return f"Tebrikler {name}"
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
